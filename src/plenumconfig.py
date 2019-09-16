09a18fc034096347ba0d844f3f63040147360806
Andre <andre@dev-town.de>
2019-09-16 20:18:01 +0200

    add config

---
+++ src/plenumconfig.py
@@ -0,0 +1,30 @@
+### Etherpad ###
+myapikey = ''
+mybaseURL = ''
+mypadID = 'PADNAME'
+mypadIDFalse = "FALSEPADNAME"
+
+#falsPadText = """
+#  ,-~~-.___.
+# / |  '     \         It was a dark and stormy night....
+#(  )         0          use the Padnames only in lowercase ;-)
+# \_/-, ,----'
+#    ====           //
+#   /  \-'~;    /~~~(O)
+#  /  __/~|   /       |
+#=(  _____| (_________|   W<"""
+
+
+saveSpace = '../reports/'
+
+### wiki ###
+wikiurl = ""
+wikiuser = ""
+wikipassword = ""
+wikiKategorie = ""
+wikiwikiprefix = ""
+
+### Stuff ###
+
+filePrefix = ""
+wikiprefix = filePrefix
